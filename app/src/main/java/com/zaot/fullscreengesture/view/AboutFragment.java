be1a58e4cb89e24544a209c2b87193b1a607ee9c
wutongke <ximu470791413@163.com>
2018-08-26 10:01:58 +0800

    update about ui

--- app/src/main/java/com/zaot/fullscreengesture/view/AboutFragment.java
+++ app/src/main/java/com/zaot/fullscreengesture/view/AboutFragment.java
@@ -8,12 +8,14 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import com.zaot.fullscreengesture.BuildConfig;
 import com.zaot.fullscreengesture.R;
 import com.zaot.fullscreengesture.databinding.AboutFragmentBinding;
 
 public class AboutFragment extends Fragment {
 
     private AboutFragmentBinding aboutFragmentBinding;
+    public String version;
 
     @Nullable
     @Override
@@ -22,5 +24,12 @@ public class AboutFragment extends Fragment {
                              @Nullable Bundle savedInstanceState) {
         aboutFragmentBinding = DataBindingUtil.inflate(inflater, R.layout.about_fragment, container, false);
         return aboutFragmentBinding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        version = getResources().getString(R.string.gesture_control_version, BuildConfig.VERSION_NAME);
+        aboutFragmentBinding.setModel(this);
     }
 }
